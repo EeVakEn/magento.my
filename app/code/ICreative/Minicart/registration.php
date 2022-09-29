55ed291bd2e771521828289e64cc1b6681e87071
Danil Ivakhin <danucha129@gmail.com>
2022-09-29 13:12:44 +0600

    header minicart

---
+++ app/code/ICreative/Minicart/registration.php
@@ -0,0 +1,5 @@
+<?php
+
+use Magento\Framework\Component\ComponentRegistrar;
+
+ComponentRegistrar::register(ComponentRegistrar::MODULE, 'ICreative_Minicart', __DIR__);
---
+++ app/code/ICreative/Search/view/frontend/web/js/search.js
@@ -0,0 +1,18 @@
+const searchButton = document.querySelector('.search-link')
+const popupSearch = document.querySelector('.search-box')
+let isShow = false
+
+searchButton.addEventListener('click',(e)=>{
+    e.preventDefault()
+    if (isShow === false){
+        popupSearch.classList.add('show')
+        searchButton.style.setProperty("--display", "inline-block")
+        isShow=true
+    }else{
+        popupSearch.classList.remove('show')
+        searchButton.style.setProperty("--display", "none")
+        isShow=false
+    }
+})
+
+
